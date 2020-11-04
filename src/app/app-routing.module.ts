c3a4eb150a5f0463b07b9ccc5c60ead8b483bbaf
arnobl <1052622+arnobl@users.noreply.github.com>
2020-11-04 15:18:54 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {C1Component} from './component/c1/c1.component';
+import {AppComponent} from './app.component';
 
 
 const routes: Routes = [
+  { path: '', component: AppComponent },
   { path: 'c1', component: C1Component },
   // { path: 'c2', component: C2Component },
   { path: '',
-    redirectTo: '/c1', // auto redirect to /c1
+    redirectTo: '/', // auto redirect to /c1
     pathMatch: 'full'
   }
 ];
